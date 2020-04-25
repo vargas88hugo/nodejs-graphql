4cd644c859b580def9b09c4bd08b07296c029422
Hugo Vargas <vargas88hugo@gmail.com>
2020-04-24 23:18:43 -0500

    [Feature] Implemented graphql schema with buildSchema and created a mock database

---
+++ crud-graphql/courses.js
@@ -0,0 +1,4 @@
+module.exports = [
+  { id: '1', title: 'First Course', views: 1000 },
+  { id: '2', title: 'Second Course', views: 5000 },
+];
--- crud-graphql/server.js
+++ crud-graphql/server.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const { buildSchema } = require('graphql');
+
+const courses = require('./courses');
 
 const app = express();
 
+// ! means is obligatory and ID is a unique id
+const schema = buildSchema(`
+  type Course {
+    id: ID!
+    title: String!
+    views: Int
+  }
+
+  type Query {
+    getCourses: [Course]
+  }
+`);
+
 app.get('/', (req, res) => {
-  res.send('Welcome');
+  res.json(courses);
 });
 
 app.listen(5000, () => {
