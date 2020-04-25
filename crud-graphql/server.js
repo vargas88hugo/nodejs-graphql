3dde86ed7a4f227917d68f51385b8f2e27fe938f
Hugo Vargas <vargas88hugo@gmail.com>
2020-04-25 00:08:04 -0500

    [Update] Added query get course by id and mutators such as update and add course

--- crud-graphql/server.js
+++ crud-graphql/server.js
@@ -16,12 +16,37 @@ const schema = buildSchema(`
 
   type Query {
     getCourses: [Course]
+    getCourse(id: ID!): Course
+  }
+
+  type Mutation {
+    addCourse(title: String!, views: Int): Course
+    updateCourse(id: ID!, title: String!, views: Int): Course
   }
 `);
 
 const root = {
   getCourses() {
     return courses;
+  },
+  getCourse({ id }) {
+    return courses.find((course) => id == course.id);
+  },
+  addCourse({ title, views }) {
+    const id = String(courses.length + 1);
+    const course = { id, title, views };
+
+    courses.push(course);
+
+    return course;
+  },
+  updateCourse({ id, title, views }) {
+    const course = courses.find((course) => course.id == id);
+
+    course.title = title;
+    course.views = views;
+
+    return course;
   },
 };
 
