b14b53af09234f41ed214abef1ffa382abe862d5
Hugo Vargas <vargas88hugo@gmail.com>
2020-04-26 18:39:08 -0500

    [Update] Implemented hashed password with encryption

--- db/models/user.js
+++ db/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const Schema = mongoose.Schema;
 
@@ -14,6 +15,19 @@ const UserSchema = new Schema({
       ref: 'Course',
     },
   ],
+});
+
+UserSchema.virtual('password');
+
+UserSchema.pre('validate', async function () {
+  if (this.password === undefined) return;
+
+  try {
+    this.hashedPassword = await bcrypt.hash(this.password, 10);
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 });
 
 module.exports = User = mongoose.model('User', UserSchema);
